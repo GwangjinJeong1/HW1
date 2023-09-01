d35f6be1488646f040edf21b9212ceab2caf09c8
GwangjinJeong <21900651@handong.ac.kr>
2023-09-01 16:49:36 +0900

    make Project1

---
+++ src/main/java/com/myapp/word/Student.java
@@ -0,0 +1,36 @@
+package com.myapp.word;
+
+public class Student {
+    private String stdId;
+    private String name;
+
+    public Student(String stdId, String name) {
+        this.stdId = stdId;
+        this.name = name;
+    }
+
+    public Student() {
+
+    }
+
+    public String getStdId() {
+        return stdId;
+    }
+
+    public void setStdId(String stdId) {
+        this.stdId = stdId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student : " + stdId + " - " + name;
+    }
+}
